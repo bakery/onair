38a407f4a349675c7c58e50a918a1233212e7424
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-01-04 20:14:29 +0200

    introduce room controller and view

--- client/controllers/rooms.js
+++ client/controllers/rooms.js
@@ -1,3 +1,4 @@
+// list of rooms
 RoomsController = RouteController.extend({
     template: 'rooms',
 
@@ -15,6 +16,32 @@ RoomsController = RouteController.extend({
     data: function () {
         return {
             rooms : Rooms.find({})
+        };
+    }
+
+    // action: function () {
+    //  if we want to override default behavior 
+    // }
+});
+
+//specific room
+RoomController = RouteController.extend({
+    template: 'room',
+
+    before: function () {
+
+    },
+
+    after: function () {
+    },
+
+    waitOn: function () {
+        return Meteor.subscribe('rooms');
+    },
+
+    data: function () {
+        return {
+            room : Rooms.findOne({ _id : this.params.id })
         };
     }
 
--- client/router.js
+++ client/router.js
@@ -1,15 +1,20 @@
 Router.configure({
-  layoutTemplate: 'layout'
+    layoutTemplate: 'layout'
 });
 
 Router.map(function () {
-  this.route('landing', {
-    path: '/',
-    controller : 'LandingController'
-  });
+    this.route('landing', {
+        path: '/',
+        controller : 'LandingController'
+    });
 
-  this.route('rooms', {
-  	path: '/rooms',
-  	controller: 'RoomsController'
-  });
+    this.route('rooms', {
+        path: '/rooms',
+        controller: 'RoomsController'
+    });
+
+    this.route('room', {
+        path: '/room/:id',
+        controller: 'RoomController'
+    });
 });
