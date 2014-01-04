a2b709705057f8e4beb3e95c625f90b5249e7f76
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-01-05 01:22:36 +0200

    add data endpoint for querying user's favorites on SC

--- client/models/soundcloud.js
+++ client/models/soundcloud.js
@@ -1,14 +1,26 @@
+Soundcloud = {
+    getFavorites : function(callback){
+        SC.get("/me/favorites", function(response){
+            if(callback){
+                callback(response);
+            }
+
+            Session.set('sc.favorites',response);
+        });
+    }
+};
+
 // set access token on the SC instance once someone logs in
 
 Deps.autorun(function () {
-	var user = Meteor.user();
-	if(user){
-		if(user.services.soundcloud){
-			var accessToken = user.services.soundcloud.accessToken;
-			if(accessToken){
-				console.log('setting access token');
-				SC.accessToken(accessToken);
-			}
-		}
-	}
+    var user = Meteor.user();
+    if(user){
+        if(user.services.soundcloud){
+            var accessToken = user.services.soundcloud.accessToken;
+            if(accessToken){
+                console.log('setting access token');
+                SC.accessToken(accessToken);
+            }
+        }
+    }
 });
