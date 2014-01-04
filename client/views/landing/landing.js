d12701d6f04e510cf7fb2ad12819d5b35356d09c
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-01-04 18:23:07 +0200

    navigate to the rooms when logged in

--- client/views/landing/landing.js
+++ client/views/landing/landing.js
@@ -1,9 +1,18 @@
+
+var handleOAuth = function(e){
+	if(typeof e !== 'undefined'){
+		alert('Something went terribly wrong. Run around scream and shout.');
+	} else {
+		Router.go('rooms');
+	}
+};
+
 Template.landing.events = {
 	'click .login-sc' : function(){
-		Meteor.loginWithSoundcloud();
+		Meteor.loginWithSoundcloud({}, handleOAuth);
 	},
 
 	'click .login-fb' : function(){
-		Meteor.loginWithFacebook();
+		Meteor.loginWithFacebook({}, handleOAuth);
 	}
 };
