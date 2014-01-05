02c21b8ebdab8c7153f37a11821c212d3d800687
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-01-05 13:53:33 +0200

    wire sound playback - kick back and enjoy

--- client/views/soundcloud/favorites.js
+++ client/views/soundcloud/favorites.js
@@ -1,3 +1,10 @@
 Template['sc-favorites'].favorites = function(){
 	return Session.get('sc.favorites');
 };
+
+Template['sc-favorites'].events = {
+	'click .stream-button' : function(event, template){
+		var url = $(event.currentTarget).data('url');
+		Sounds.createSoundFromSoundCloud({ stream_url : url }).play();
+	}
+};
