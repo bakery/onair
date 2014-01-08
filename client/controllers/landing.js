cd4b22e9c10aaf3118e1202ab536fdd89ffbf2c2
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-01-09 00:06:38 +0200

    track authentication status and redirect authenticated peeps straight to the rooms

--- client/controllers/landing.js
+++ client/controllers/landing.js
@@ -1,3 +1,15 @@
 LandingController = RouteController.extend({
-    template: 'landing'
+    template: 'landing',
+
+    before : function(){
+        Deps.autorun(function () {
+            if(Meteor.user()){
+                Router.go('rooms');
+            }
+        });
+    },
+
+    after : function(){
+        this.render('signin',{ to : 'auth'});
+    }
 });
