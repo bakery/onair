abb8e34c4a0c1a6245ae3739961bf8c2dd624cbb
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-01-08 11:41:45 +0200

    make sure the person is authenticated to see the room

--- client/router.js
+++ client/router.js
@@ -2,6 +2,18 @@ Router.configure({
     layoutTemplate: 'layout'
 });
 
+
+var filters = {
+    isLoggedIn: function() {
+        if (!(Meteor.loggingIn() || Meteor.user())) {
+            this.render('signin');
+            this.stop();
+        }
+    }
+};
+
+Router.before(filters.isLoggedIn, {only: ['room']});
+
 Router.map(function () {
     this.route('landing', {
         path: '/',
