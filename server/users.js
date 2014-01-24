437f96a70b42f8a56548415a9acb8a1a15f58f62
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-01-24 19:44:39 +0200

    initialize authentication only based on settings

--- server/users.js
+++ server/users.js
@@ -1,53 +1,55 @@
-//setup login service config
-Meteor.startup(function () {
-    Accounts.loginServiceConfiguration.remove({
-        service: "soundcloud"
-    });
-
-    Accounts.loginServiceConfiguration.insert({
-        service: "soundcloud",
-        clientId : Meteor.settings.soundcloud.clientId,
-        secret : Meteor.settings.soundcloud.secret
-    });
-
-    Accounts.loginServiceConfiguration.remove({
-        service: "facebook"
-    });
-
-    Accounts.loginServiceConfiguration.insert({
-        service: "facebook",
-        appId: Meteor.settings.facebook.appId,
-        secret: Meteor.settings.facebook.secret
-    });
-});
-
-// when a new user joins, update her profile based on data from external services
-Accounts.onCreateUser(function(options, user) {
-
-  if(typeof user.services !== 'undefined'){
-    if(typeof user.services.soundcloud !== 'undefined'){
-        var scProfile = user.services.soundcloud;
-        user.profile = _.extend({}, {
-            picture : scProfile.avatar_url,
-            city : scProfile.city,
-            country : scProfile.country,
-            name : scProfile.full_name,
-            soundcloud : {
-                id : scProfile.id,
-                permalink : scProfile.permalink_url
-            }
+if(Meteor.settings.authentication){
+    //setup login service config
+    Meteor.startup(function () {
+        Accounts.loginServiceConfiguration.remove({
+            service: "soundcloud"
         });
-    }
 
-    if(typeof user.services.facebook !== 'undefined'){
-        var fbProfile = user.services.facebook;
-        user.profile = _.extend({}, {
-            name : fbProfile.name,
-            picture : 'http://graph.facebook.com/' +  fbProfile.id + '/picture?width=100&height=100'
+        Accounts.loginServiceConfiguration.insert({
+            service: "soundcloud",
+            clientId : Meteor.settings.authentication.soundcloud.clientId,
+            secret : Meteor.settings.authentication.soundcloud.secret
         });
-    }
 
-  }
+        Accounts.loginServiceConfiguration.remove({
+            service: "facebook"
+        });
 
-  return user;
-});
+        Accounts.loginServiceConfiguration.insert({
+            service: "facebook",
+            appId: Meteor.settings.authentication.facebook.appId,
+            secret: Meteor.settings.authentication.facebook.secret
+        });
+    });
+
+    // when a new user joins, update her profile based on data from external services
+    Accounts.onCreateUser(function(options, user) {
+
+      if(typeof user.services !== 'undefined'){
+        if(typeof user.services.soundcloud !== 'undefined'){
+            var scProfile = user.services.soundcloud;
+            user.profile = _.extend({}, {
+                picture : scProfile.avatar_url,
+                city : scProfile.city,
+                country : scProfile.country,
+                name : scProfile.full_name,
+                soundcloud : {
+                    id : scProfile.id,
+                    permalink : scProfile.permalink_url
+                }
+            });
+        }
+
+        if(typeof user.services.facebook !== 'undefined'){
+            var fbProfile = user.services.facebook;
+            user.profile = _.extend({}, {
+                name : fbProfile.name,
+                picture : 'http://graph.facebook.com/' +  fbProfile.id + '/picture?width=100&height=100'
+            });
+        }
+
+      }
+
+      return user;
+    });
+}
